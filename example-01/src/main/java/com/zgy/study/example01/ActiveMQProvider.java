0d5c506e85ea9c97cb334de899623f0e30f15664
ZGY <3030392760@qq.com>
2019-12-13 09:56:44 +0800

    完善代码

--- example-01/src/main/java/com/zgy/study/example01/ActiveMQProvider.java
+++ example-01/src/main/java/com/zgy/study/example01/ActiveMQProvider.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
-
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
